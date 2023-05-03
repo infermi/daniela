a061a9c53825edc064bceda109ababdc9732ca38
infermi <80839246+infermi@users.noreply.github.com>
2023-05-03 15:13:31 +0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                'varela': 'Varela Round',
+                'sans': 'Open Sans',
+                'heebo': 'Heebo'
+            },
+            colors: {
+                'primary': '#4DB782',
+                'dark_primary': '#3C9469',
+                'text_color': '#FCF0F3',
+                'home': '#dbf1e6',
+
+            },
+
+            variants: {
+                animation: ["motion-safe"]
+            },
+            animation: {
+                fadeIn: "fadeIn 0.2s ease-in forwards "
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: 0, transform: 'translate(0, 30px)' },
+                    "100%": { opacity: 1 }
+                }
+            }
+        },
+
+    },
+    plugins: [require("tailwindcss-animation-delay")],
+}
