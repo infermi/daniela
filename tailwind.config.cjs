84c3e8b422c911a2a0d016e98a164e74983acb16
infermi <80839246+infermi@users.noreply.github.com>
2023-05-05 10:51:38 +0300

    added reviews, improved stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,12 +21,18 @@ module.exports = {
                 animation: ["motion-safe"]
             },
             animation: {
-                fadeIn: "fadeIn 0.2s ease-in forwards "
+                fadeIn: "fadeIn 0.2s ease-in forwards ",
+                scrollDown: "scrollDown 0.4 ease-in"
             },
             keyframes: {
                 fadeIn: {
                     "0%": { opacity: 0, transform: 'translate(0, 30px)' },
                     "100%": { opacity: 1 }
+                },
+                scrollDown: {
+                    "0%": {},
+                    "50%": { transform: 'translate(0,30px)' },
+                    "100%": {}
                 }
             }
         },
